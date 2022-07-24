fc9afd0fc5381383c679341951ccd23c37bc79ee
michael <kimh060612@naver.com>
2022-07-25 00:10:37 +0900

    .

--- telegramConnection.py
+++ telegramConnection.py
@@ -6,10 +6,11 @@ async def getTelegramClient(api_id, api_hash, username, phone):
     await client.start()
     print("Client Created")
     # Ensure you're authorized
-    if not client.is_user_authorized():
-        client.send_code_request(phone)
+    isAuthenticated = await client.is_user_authorized()
+    if not isAuthenticated:
+        await client.send_code_request(phone)
         try:
-            client.sign_in(phone, input('Enter the code: '))
+            await client.sign_in(phone, input('Enter the code: '))
         except SessionPasswordNeededError:
-            client.sign_in(password=input('Password: '))
+            await client.sign_in(password=input('Password: '))
     return client
