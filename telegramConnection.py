0f27d9e879b4cd791a18c99c7bfcc878412a252d
michael <kimh060612@naver.com>
2022-07-25 00:21:09 +0900

    .

--- telegramConnection.py
+++ telegramConnection.py
@@ -3,14 +3,14 @@ from telethon.errors import SessionPasswordNeededError
 
 async def getTelegramClient(api_id, api_hash, username, phone):
     client = TelegramClient(username, api_id, api_hash)
-    await client.start()
+    await client.start(phone)
     print("Client Created")
     # Ensure you're authorized
     isAuthenticated = await client.is_user_authorized()
     if not isAuthenticated:
-        client.send_code_request(phone)
+        await client.send_code_request(phone)
         try:
-            client.sign_in(phone, input('Enter the code: '))
+            await client.sign_in(phone, input('Enter the code: '))
         except SessionPasswordNeededError:
-            client.sign_in(password=input('Password: '))
+            await client.sign_in(password=input('Password: '))
     return client
