bc0e38aeb0ddc09d898992113c7f4185cdb26bca
michael <kimh060612@naver.com>
2022-08-01 00:10:35 +0900

    .

--- telegramCrawler.py
+++ telegramCrawler.py
@@ -34,6 +34,7 @@ async def main():
                            password=db_password,
                            db=db_database) as cRepository:
         channelList = cRepository.getChannelList(table=table)
+        print(channelList)
         for channel_id, channel_name in channelList:
             print('----------------------{}-----------------------'.format(channel_name))
             real_id, _ = utils.resolve_id(channel_id)
