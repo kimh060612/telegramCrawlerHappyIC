c7adf666383aa01e87d7618bcfb625c0beb0b895
michael <kimh060612@naver.com>
2022-09-01 23:31:19 +0900

    .

--- telegramCrawlingTest.py
+++ telegramCrawlingTest.py
@@ -25,13 +25,7 @@ config.read('./config.ini')
 file_directory_1 = config['FileDirectory']['first_directory']
 file_directory_2 = config['FileDirectory']['second_directory']
 
-def findChannelId(channel, channelList):
-    for channel_id, channel_name, _ in channelList:
-        if channel_name == channel:
-            return channel_id
-    return False
-
-async def telegramCrawler(channel: str):
+async def main():
     table = args.table
     db_username, db_password, db_host, db_port, db_database = getDatabaseConfig()
     with ChannelRepository(host=db_host, 
@@ -41,15 +35,19 @@ async def telegramCrawler(channel: str):
                            db=db_database) as cRepository:
         channelList = cRepository.getChannelWhiteList(table=table)
         print(channelList)
-        channel_id = findChannelId(channel, channelList)
-        if channel_id:
-            raise ValueError("Invalid Channel")
         
-        print('----------------------{}-----------------------'.format(channel))
-        real_id, _ = utils.resolve_id(int(channel_id))
-        async for message in client.iter_messages(PeerChannel(real_id), limit=30):
-            print(message.to_dict())
-        print('----------------------{}-----------------------'.format('-' * len(channel)))
+        for channel_id, channel_name, _ in channelList:
+            print('----------------------{}-----------------------'.format(channel_name))
+            real_id, _ = utils.resolve_id(int(channel_id))
+            async for message in client.iter_messages(PeerChannel(real_id), limit=30):
+                print(message.id, message.chat.title, message.text)
+                print()
+            print('----------------------{}-----------------------'.format('-' * len(channel_name)))
+        
+
+if __name__ == "__main__":
+    client.loop.run_until_complete(main())
+    
 
     
     
