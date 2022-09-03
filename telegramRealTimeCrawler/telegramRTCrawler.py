529704ea6b96ccf4cbb0f95a2905e0c51cc3a7a4
michael <kimh060612@naver.com>
2022-09-03 23:55:10 +0900

    .

--- telegramRealTimeCrawler/telegramRTCrawler.py
+++ telegramRealTimeCrawler/telegramRTCrawler.py
@@ -7,7 +7,7 @@ from telethon import events
 from elasticsearch import Elasticsearch
 from telegramConnection import getTelegramClient, getTelegramConfig
 from telethon.tl.types import PeerChannel
-import asyncio
+import json
 
 es = Elasticsearch('http://localhost:9200', basic_auth=('elastic', 'H@ppYiCP@sswD9!*2'))
 parser = argparse.ArgumentParser(description="Telegram Chatting Crawler for HappyIC Project")
@@ -22,8 +22,25 @@ client = getTelegramClient(API_ID, API_HASH, USERNAME, PHONE)
 
 @client.on(events.NewMessage(chats=Channel))
 async def newMessageListener(event):
-    newMessage = event.message
-    print(newMessage)
+    msg = event.message
+    elastic_msg = {
+        "id": str(msg["id"]),
+        "date": msg["date"].strftime('%Y-%m-%d %H:%M'),
+        "content": msg["message"],
+        "mentioned": msg["mentioned"],
+        "forwards": msg["forwards"]
+    }
+    if not msg["fwd_from"] is None:
+        try :
+            elastic_msg["fwd_msg_channel"] = str(msg["fwd_from"]["from_id"]["channel_id"])
+            elastic_msg["fwd_msg_id"] = str(msg["fwd_from"]["channel_post"])
+            elastic_msg["fwd_msg_date"] = msg["fwd_from"]["date"].strftime('%Y-%m-%d %H:%M')
+        except :
+            print(msg)
+    print(json.dumps(elastic_msg, indent=4))
+    _ = es.index(index='telegram', document=elastic_msg)
+    print("\n")
+    print(msg)
     
 if __name__ == "__main__":
     client.run_until_disconnected()
